bb29de7152682e79dc53004996b725fcc9aed5b9
Asura <bankaikinowork@gmail.com>
2025-01-04 00:21:18 +0300

    some styles

--- src/components/Bankai.jsx
+++ src/components/Bankai.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import KinoboxPlayer from "./KinoboxPlayer/KinoboxPlayer";
 import RandomAnime from "./RandomAnime";
+import { Button, Modal } from "antd";
 
 export default function Bankai() {
   const [message, setMessage] = useState("");
@@ -9,9 +10,16 @@ export default function Bankai() {
     setMessage(event.target.value);
   };
 
-  function find(lol) {
-    setMessage(lol);
-  }
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <>
@@ -20,14 +28,26 @@ export default function Bankai() {
           <label className="text-field__label">
             Write kinopoisk/imdb id or name:
           </label>
-
           <input
             onChange={handleChange}
             value={message}
             className="text-field__input"
             placeholder="kinopoisk/imdb id or name"
             autocomplete="off"
-          />
+          />{" "}
+          <Button
+            type="primary"
+            onClick={showModal}
+            style={{
+              display: "inline-block",
+              backgroundColor: "#9a6106",
+              fontSize: "1rem",
+              fontWeight: "400",
+              height: "calc(2.25rem + 2px)",
+            }}
+          >
+            instruction
+          </Button>
         </div>
         {/* <button onClick={() => find(message)}>press</button> */}
         <div>
@@ -48,6 +68,26 @@ export default function Bankai() {
         {/* <EffectsSection></EffectsSection> */}
         <RandomAnime></RandomAnime>
         <br></br>
+        <Modal
+          title="How to use it"
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <p>
+            1. There are several different players working for you, and you can
+            select any of them from the right menu. If one doesn't work, try the
+            other.
+          </p>
+          <p>
+            2. In the upper-left menu, you can select the season, episode, and
+            voice acting.
+          </p>
+          <p>
+            3. The preferred search parameters are the imdb and kinopoisk IDs.
+            The name search also works, but not as well as we would like.
+          </p>
+        </Modal>
         <div style={{ visibility: "hidden" }}>
           <h1>lkjnl</h1>
           <h1>lkjnl</h1>
